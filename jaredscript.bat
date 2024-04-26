50499beaa72405507cb359bbf53d506989788ef2
michael-miftek <michael@miftek.com>
2024-04-25 23:04:35 -0400

    all changes

--- jaredscript.bat
+++
@@ -1,7 +0,0 @@
-echo off
-REM - put your file location here
-set LOC=c:\users\jared\source\reports\granite_repo\granite\granite01
-set VENV=c:\users\jared\source\repos\gproto_1_3\gproto\venv\Scripts\activate
-
-%VENV%
-cd %LOC%
