989fa12b03c62d5d2a2c50285926d1f3792971d7
michael-miftek <michael@miftek.com>
2024-07-10 21:31:48 -0400

    update with output almost done

---
+++ services/buffOut.py
@@ -0,0 +1,35 @@
+import time
+import socket
+import pickle
+import random
+import os
+import sys
+import numpy as np
+
+
+def read():
+    with open('daqbuff.npy', 'rb') as file:
+        a = np.load(file)
+        file.close()
+        return a
+
+if __name__ == "__main__":
+    
+    #Use this same 3 lines and change the connect function to bind that will set up the receiving side
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.connect(('127.0.0.1', 5000))
+    sock.settimeout(5)
+    
+    arr = read()
+    
+    
+    while True:
+        #Using a try catch to easily debug at this time but if there is a timeout error could just be told to continue
+        data = pickle.dumps(arr[random.randint(0,64)])
+        # data = pickle.dumps(random.randint(0,64))
+        try:
+            # sock.sendall(pickle.dumps(arr[random.randint(0,64)]))
+            sock.sendall(data)
+        except Exception as e:
+            print(e)
+            break
---
+++ services/test.py
@@ -0,0 +1,31 @@
+import time
+import socket
+import pickle
+import random
+import os
+import sys
+import numpy as np
+
+
+if __name__ == "__main__":
+    
+    #We use bind on this side of life
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(('127.0.0.1', 5000))
+    sock.settimeout(5)
+    start = time.time() + 1
+    c = 0
+    while True:
+        #Using a try catch to easily debug at this time but if there is a timeout error could just be told to continue
+        try:
+            #12 channels of 1250 uint16s should be 30000 bytes so 65536 should be more than enough
+            _ = sock.recv(65536)
+            c+=1
+        except Exception as e:
+            print(e)
+            continue
+        if start < time.time():
+            start += 1
+            print(c)
+            c=0
+            # break
