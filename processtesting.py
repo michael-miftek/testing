5825118742b9cd8fc8c79f6fd4c45e36bf207a62
michael-miftek <michael@miftek.com>
2024-11-09 15:52:52 -0500

    update

--- processtesting.py
+++ processtesting.py
@@ -2,21 +2,52 @@
 import time
 
 
-def loop(i):
-    while i.value <= 510:
-        print(i.value)
-        i.value += 1
-        time.sleep(1)
+# def loop(i):
+#     while i.value <= 510:
+#         print(i.value)
+#         i.value += 1
+#         time.sleep(1)
 
-if __name__ == "__main__":
-    # NOTE: Value and Array are options but it looks like Manager allows us to use python types and 
-    #       alter them as such so to use a dict attribute Manager might be the move
-    from multiprocessing import Process, Value
+# if __name__ == "__main__":
+#     # NOTE: Value and Array are options but it looks like Manager allows us to use python types and 
+#     #       alter them as such so to use a dict attribute Manager might be the move
+#     from multiprocessing import Process, Value
 
-    i = Value("i", 1)  # "i" for integer, initial value 1
-    p = Process(target=loop, args=(i,))
-    p.start()
-    for base in range(100, 600, 100):
-        time.sleep(2)
-        print(f'base: {base}')
-        i.value = base
+#     i = Value("i", 1)  # "i" for integer, initial value 1
+#     p = Process(target=loop, args=(i,))
+#     p.start()
+#     for base in range(100, 600, 100):
+#         time.sleep(2)
+#         print(f'base: {base}')
+#         i.value = base
+
+from multiprocessing import Process, Manager
+
+def f(d, l):
+    d[1] = '1'
+    d['2'] = 2
+    d[0.25] = None
+    for t in range(50):
+        d[1] += '1'
+        d['2'] += 1
+        d[0.25] = None
+        time.sleep(.5)
+    d[3] = 5
+    l.reverse()
+
+if __name__ == '__main__':
+    with Manager() as manager:
+        d = manager.dict()
+        l = manager.list(range(10))
+
+        p = Process(target=f, args=(d, l))
+        p.start()
+        for i in range(10):
+            print("hi")
+            time.sleep(1)
+            print(d)
+        
+        p.join()
+
+        print(d)
+        print(l)
